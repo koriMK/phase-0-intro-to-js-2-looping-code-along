c3de70ef96f1738d2f6f1d3ff2beb19dd0ce5d7e
Joseph Mwangi <korimwangi01@gmail.com>
2025-06-13 05:48:37 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+
+const cards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(cards);
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+
+countDown(10);
